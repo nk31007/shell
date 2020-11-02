3de6e3f4fa79f86037f92238d72411666c5b04ac
nk31007 <sys.nag.adm@gmail.com>
2020-11-02 07:39:15 +0000

    Script Ready Now

--- cpumemdisk.sh
+++ cpumemdisk.sh
@@ -3,10 +3,24 @@
 # Author: Nageswara Rao Kallutla
 # Website: server-compute.com
 ## format : Hostname, Date&Time, %CPU, Mem%, Disk%
+set -xe
 
 HOSTNAME=$(hostname)
 DATE=$(date "+%Y-%m-%d %H:%M:%S")
 CPUUSAGE=$(top -b -n1 -d1|grep "Cpu(s)"|awk '{print $2}'|awk -F. '{print $1}')
 MEMUSAGE=$(free -m| grep Mem| awk '{print ($3/$2)*100}')
-DISKUSAGE=$()
+DISKUSAGE=$(df -Ph| column -t| awk '{print $5}' |tail -n 1|sed 's/%//g')
+
+echo -e  "Hostname,\t\t Date&Time, \t\t%CPU, \t\tMem%, \t\tDisk%" >>/opt/usagereport
+echo -e  "$HOSTNAME, \t$DATE, \t$CPUUSAGE, \t\t$MEMUSAGE, \t$DISKUSAGE" >>/opt/usagereport
+for i in `cat /scripts/hostlist`
+do
+RHOSTNAME=$(ssh $i hostname)
+RDATE=$(date "+%Y-%m-%d %H:%M:%S")
+RCPUUSAGE=$(top -b -n1 -d1|grep "Cpu(s)"|awk '{print $2}'|awk -F. '{print $1}')
+RMEMUSAGE=$(free -m| grep Mem| awk '{print ($3/$2)*100}')
+RDISKUSAGE=$(df -Ph| column -t| awk '{print $5}' |tail -n 1|sed 's/%//g')
+
+echo -e  "$RHOSTNAME, \t$RDATE, \t$RCPUUSAGE, \t\t$RMEMUSAGE, \t$RDISKUSAGE" >>/opt/usagereport
+done
 
